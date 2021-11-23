c8399e68698a234fb4bb60a45ad161a6adf7eed3
inteoryx <inteoryx@protonmail.com>
2021-11-23 14:47:05 -0800

    StumblingOn Redesign1 without mobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,204 @@
-import logo from './logo.svg';
 import './App.css';
+import like from './like.svg';
+import liked from './liked.svg';
+import React from 'react';
+import sc from './StumbleClient';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      history: [],
+    };
+  }
+
+  componentDidMount() {
+    sc.getHistory(
+      0,
+      (s) => this.setState({history: s.results}),
+      (e) => console.log(e)
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Stumble pushHistory={(h) => {
+          console.log(h);
+          this.setState({history: [{url: h.url, liked: false, dislike: false}, ...this.state.history]});
+          console.log(this.state.history);
+        }} />
+        <About />
+        <History history={this.state.history} />
+        <Ad />
+      </div>
+    );
+  }
+}
+
+class Stumble extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prevId: "",
+      prevUrl: "",
+      nextUrl: "",
+      nextId: "",
+      submissionUrl: "",
+      submissionPlaceholder: "https://your.url.here",
+      modal: false,
+      modalMessage: "This is a test.",
+    };
+
+    this.updateSite = this.updateSite.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateSite();
+  }
+
+  updateSite() {
+    if(this.state.nextId) {
+      this.props.pushHistory({id: this.state.nextId, url: this.state.nextUrl});
+    }
+
+    sc.getSite(
+      this.state.nextId, 
+      (s) => {this.setState({nextUrl: s.url, nextId: s.siteId, prevId: this.state.nextId, prevUrl: this.state.nextUrl})},
+      (e) => {console.log(e)},
+    );
+  }
+
+  render() {
+    return (
+      <div id="stumbleSection">
+        {this.state.modal && <Modal message={this.state.modalMessage} closeModal={() => this.setState({modal: false})} />}
+        <button 
+          id="stumbleButton"
+          className="button"
+          onClick={() => {
+            window.open(this.state.nextUrl);
+            this.updateSite();
+          }}
+        >STUMBLE</button>
+        <div id="submissionPanel">
+          <input 
+            type="text" 
+            id="submissionInput"
+            placeholder={this.state.submissionPlaceholder}
+            value={this.state.submissionUrl}
+            onChange={(e) => {this.setState({submissionUrl: e.target.value.trim()})}} 
+          />
+          <button 
+            id="submitButton"
+            className="button"
+            onClick={() => {
+              sc.submitSite(
+                this.state.submissionUrl,
+                (s) => this.setState({modal: true, modalMessage: s.message, submissionUrl: ""}),
+                (e) => {
+                  console.log(e);
+                  this.setState({modal: true, modalMessage: e.message, submissionUrl: ""});
+                }
+              );
+            }}
+          >SUBMIT</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function About() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="aboutSection">
+      <h1 className="title">StumblingOn</h1>
+      <p className="text">StumblingOn shows you a random webpage whenever you click the STUMBLE button. Bored? Why not stumble on something unique?  StumblingOn is a stumble upon alternative, or clone.</p>
+      <p className="text">You'll stumble on pages that are UNIQUE. Our catalog is interesting, independent, and different. We want to recapture the old, weird, and adventurous feeling of the web.</p>
+      <p className="text">Submit a website. Submit YOUR website. If it's popular, pornographic, or professional, don't bother. If it's something you made and want to share - you're in exactly the right place. We'd love to have you.</p>
+      <p  className="text">Follow development on <a href="https://youtube.com">YouTube</a></p>
+      <div id="aboutLinks"><a href="https://service.stumblingon.com/metrics">metrics</a> <a href="https://service.stumblingon.com/docs#/">api</a> <a href="">github</a></div>
+    </div>
+  );
+}
+
+function History(props) {
+  return (
+    <div id="historySection">
+      <h1 className="title">History</h1>
+      <div id="historyList">
+        {props.history.map((item, index) => {
+          return (<HistoryItem item={item} number={index + 1} key={index} />)
+        })}
+      </div>
+    </div>
+  );
+}
+
+class HistoryItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      liked: props.item.liked,
+      disliked: props.item.disliked,
+      mouseOverLike: false,
+    };
+
+    this.like = this.like.bind(this);
+  }
+
+  like() {
+    console.log("Liking this.");
+    sc.like(
+      this.props.item.id,
+      (s) => this.setState({liked: !this.state.liked, disliked: false}),
+      (e) => console.log(e)
+    );
+  }
+
+  render() {
+    return (
+      <div className="historyItem text">
+        <div className="historySite">
+          {this.props.number}. <a href={this.props.item.url}>{this.props.item.url}</a>
+        </div>
+        <div className="historyLike">
+          <img 
+            className={this.state.mouseOverLike ? "likeIcon pulse" : "likeIcon"}
+            src={this.state.liked ? liked : like} 
+            onClick={this.like}
+            onMouseEnter={() => this.setState({mouseOverLike: true})}
+            onMouseLeave={() => this.setState({mouseOverLike: false})}
+          />
+        </div>
+        <div className="historyTalk"><a href={"https://twitter.com/search?q=" + encodeURIComponent(this.props.item.url) + "&src=typed_query&f=top"} target="_blank">Talk</a></div>
+      </div>
+    );
+  }
+}
+
+function Modal(props){
+  return (
+    <div id="overlay">
+      <div id="modal">
+        <div id="modalText">{props.message}</div>
+        <button id="modalButton" className="button" onClick={() => props.closeModal()}>OK</button>
+      </div>
+    </div>
+  )
+}
+
+function Ad() {
+  return (
+    <div id="adSection">
+      <h1 className="title">Ad</h1>
+      <p className="text">Psst.  Hey kid.  Wanna make some money?  Like, free money?</p>
+      <p className="text">Join <a href="https://crypto.com/app/9s97gpwtm3" target="_blank">crypto.com</a> with my referral code (9s97gpwtm3).  You'll get 25 dollars in cryptocurrency if you sign up.</p>
+      <p className="text">Come on.  I know you're bored.  Why not waste time and money?</p>
     </div>
   );
 }
---
+++ src/StumbleClient.js
@@ -0,0 +1,160 @@
+function hash(inp) {
+    var h = 0, i, chr;
+    if (inp.length === 0) return h;
+    for (i = 0; i < inp.length; i++) {
+      chr   = inp.charCodeAt(i);
+      h  = ((h << 5) - h) + chr;
+      h |= 0;
+    }
+    return h;
+  };
+
+function id(){
+    const curId = window.localStorage.getItem("userId");
+    if(curId){
+        return curId;
+    }
+    const newId = hash(navigator.userAgent + Date.now());
+    window.localStorage.setItem("userId", parseInt(newId));
+    return newId;
+}
+
+function StumbleClient(){
+
+    var result = {
+        url: 'https://service.stumblingon.com/',
+        id: id(),
+        defaultHistoryPageSize: 1000
+    };
+
+    result.getSite = function(prevId, onSuccess, onError){
+        fetch(this.url + 'getSite', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                'userId': this.id,
+                'prevId': prevId,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((s)=> s.json()
+            .then(function(s){
+                if(s.ok){
+                    onSuccess(s);
+                } else {
+                    onError(s);
+                }
+            }, (e)=>onError(e), (e)=>onError(e)),
+            (e)=>onError(e)
+        );
+        
+    };
+
+    result.submitSite = function(site, onSuccess, onError){
+        fetch(this.url + 'submitSite', {
+            method: 'POST',
+            mdoe: 'cors',
+            body: JSON.stringify({
+                'userId': this.id,
+                'url': site
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((s)=> s.json()
+            .then(function(s){
+                onSuccess(s);
+            }, (e)=>onError(e), (e)=>onError(e)),
+            (e)=>onError(e)
+        );
+    };
+
+    result.getUser = function(userId, onSuccess, onError){
+        fetch(this.url + 'getUser', {
+            method: 'POST',
+            mdoe: 'cors',
+            body: JSON.stringify({
+                'userId': this.id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((s)=> s.json()
+            .then(function(s){
+                if(s.ok){
+                    onSuccess(s);
+                }
+                else {
+                    console.log("Trouble getting user.", s.errorMessage);
+                    onError(s);
+                }
+            }, (e)=>onError(e), (e)=>onError(e)),
+            (e)=>onError(e)
+        );
+    };
+
+    result.getHistory = function(start, onSuccess, onError){
+        fetch(this.url + 'getHistory', {
+            method: 'POST',
+            mdoe: 'cors',
+            body: JSON.stringify({
+                'userId': this.id,
+                'start': start,
+                'pageSize': this.defaultHistoryPageSize
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((s)=> s.json()
+            .then(function(s){
+                if(s.ok){
+                    onSuccess(s);
+                }
+                else {
+                    console.log("Trouble getting history.", s.errorMessage);
+                    onError(s);
+                }
+            }, (e)=>onError(e), (e)=>onError(e)),
+            (e)=>onError(e)
+        );
+    };
+
+    result.like = function(siteId, onSuccess, onError){
+        fetch(this.url + 'like', {
+            method: 'POST',
+            mdoe: 'cors',
+            body: JSON.stringify({
+                'userId': this.id,
+                'siteId': siteId
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((s)=> s.json()
+            .then(function(s){
+                if(s.ok){
+                    onSuccess(s);
+                }
+                else {
+                    console.log("Trouble liking.", s.errorMessage);
+                    onError(s);
+                }
+            }, (e)=>onError(e), (e)=>onError(e)),
+            (e)=>onError(e)
+        );
+    };
+
+    return result;
+}
+
+
+
+
+const sc = new StumbleClient();
+export default sc;
